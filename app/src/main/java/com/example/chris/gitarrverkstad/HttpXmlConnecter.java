407e4dee511556f50ce3ed3cbe7252e1a23f0f79
Mifaho <stefan.eliasson93@gmail.com>
2017-02-19 15:55:53 +0100

    Added http connection and xml reading

---
+++ app/src/main/java/com/example/chris/gitarrverkstad/HttpXmlConnecter.java
@@ -0,0 +1,78 @@
+package com.example.chris.gitarrverkstad;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/**
+ * Created by stefa_000 on 2017-02-17.
+ */
+
+public class HttpXmlConnecter {
+    private URL url;
+    private HttpURLConnection connection;
+    private Document doc;
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public Document getDoc() {
+        return doc;
+    }
+
+    public void setDoc(Document doc) {
+        this.doc = doc;
+    }
+
+    public HttpURLConnection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(HttpURLConnection connection) {
+        this.connection = connection;
+    }
+
+    public HttpXmlConnecter(String xmlurl) throws Exception{
+        this.url = new URL(xmlurl);
+    }
+
+    public void start() throws Exception{
+        connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        doc = parseXML(connection.getInputStream());
+    }
+
+    public void stop() throws Exception{
+        connection.disconnect();
+    }
+
+    public Document parseXML(InputStream stream) throws Exception{
+        DocumentBuilderFactory objDocumentBuilderFactory = null;
+        DocumentBuilder objDocumentBuilder = null;
+        Document doc = null;
+        objDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
+        objDocumentBuilder = objDocumentBuilderFactory.newDocumentBuilder();
+        doc = objDocumentBuilder.parse(stream);
+        return doc;
+    }
+
+    @Override
+    public String toString(){
+        String temp = doc.getTextContent();
+        if(temp != null){
+            return temp;
+        }
+        return "";
+    }
+}
